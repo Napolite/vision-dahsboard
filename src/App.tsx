dfb8dbfac80f3de9a1f34ad74e6bc9b6ee661f80
Constantine <Okonemmanuelmma@gmail.com>
2026-01-09 02:02:45 +0100

    created sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Navbar from "./components/navbar";
+import SideBar from "./components/sidebar";
 
 function App() {
-  return <div className="text-[red]">hello world</div>;
+  return (
+    <div className="flex">
+      <SideBar />
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,94 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IoHomeSharp } from "react-icons/io5";
+import { IoStatsChart } from "react-icons/io5";
+import { FaWallet } from "react-icons/fa";
+import { AiFillTool } from "react-icons/ai";
 function SideBar() {
-  return <div></div>;
+  const [selected, setSelected] = useState("dash");
+  return (
+    <div className="from-[#060b26] from-[0%] to-[#1a1f37] to-[100%] bg-gradient-to-r min-h-[100vh] w-[264px] rounded-t-[30px] text-white flex flex-col items-center ml-[5px] mt-[5px]">
+      <div className="h-[50px] mb-[80px] text-center flex place-items-center w-full justify-center">
+        Vision UI Free
+      </div>
+      <div
+        className="w-full px-[16px] flex flex-col gap-y-[20px]"
+        style={{ padding: "0 16px" }}
+      >
+        <div
+          className={`flex gap-x-[20px] w-[219.5px]  px-[15px] items-center h-[54px] rounded-[10px] ${
+            selected === "dash" ? "bg-[#1a1f37]" : "bg-none"
+          }`}
+          onClick={() => setSelected("dash")}
+        >
+          <div
+            className={`h-[32px] w-[32px] ${
+              selected === "dash" ? "bg-[#0075FF]" : "bg-[#1a1f37]"
+            } flex items-center justify-center rounded-[10px] `}
+          >
+            <IoHomeSharp
+              color={selected === "dash" ? "#ffffff" : "#0075FF"}
+              size={15}
+            />
+          </div>
+          <p>Dashboard</p>
+        </div>
+        <div
+          className={`flex gap-x-[20px] w-[219.5px]  px-[15px] items-center h-[54px] rounded-[10px] ${
+            selected === "tab" ? "bg-[#1a1f37]" : "bg-none"
+          }`}
+          onClick={() => setSelected("tab")}
+        >
+          <div
+            className={`h-[32px] w-[32px] ${
+              selected === "tab" ? "bg-[#0075FF]" : "bg-[#1a1f37]"
+            } flex items-center justify-center rounded-[10px] `}
+          >
+            <IoStatsChart
+              color={selected === "tab" ? "#ffffff" : "#0075FF"}
+              size={15}
+            />
+          </div>
+          <p>Tables</p>
+        </div>
+        <div
+          className={`flex gap-x-[20px] w-[219.5px]  px-[15px] items-center h-[54px] rounded-[10px] ${
+            selected === "bill" ? "bg-[#1a1f37]" : "bg-none"
+          }`}
+          onClick={() => setSelected("bill")}
+        >
+          <div
+            className={`h-[32px] w-[32px] ${
+              selected === "bill" ? "bg-[#0075FF]" : "bg-[#1a1f37]"
+            } flex items-center justify-center rounded-[10px] `}
+          >
+            <FaWallet
+              color={selected === "bill" ? "#ffffff" : "#0075FF"}
+              size={15}
+            />
+          </div>
+          <p>Billing</p>
+        </div>
+        <div
+          className={`flex gap-x-[20px] w-[219.5px]  px-[15px] items-center h-[54px] rounded-[10px] ${
+            selected === "rtl" ? "bg-[#1a1f37]" : "bg-none"
+          }`}
+          onClick={() => setSelected("rtl")}
+        >
+          <div
+            className={`h-[32px] w-[32px] ${
+              selected === "rtl" ? "bg-[#0075FF]" : "bg-[#1a1f37]"
+            } flex items-center justify-center rounded-[10px] `}
+          >
+            <AiFillTool
+              color={selected === "rtl" ? "#ffffff" : "#0075FF"}
+              size={15}
+            />
+          </div>
+          <p>RTL</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
